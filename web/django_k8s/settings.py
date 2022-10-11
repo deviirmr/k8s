181910bd3c3c0b7e931231503d24c6e94078c64b
deviirmr <deviirmr@gmail.com>
2022-10-11 13:54:55 +0200

    Added post model and test

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
-DEBUG = True
+
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
 ALLOWED_HOSTS = []
 if ENV_ALLOWED_HOST:
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #external
     'storages',
+    #internal
+    'posts'
 
 ]
 
